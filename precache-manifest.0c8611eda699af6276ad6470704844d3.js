11200ca734321928197ffcb60d9dbf23c902724c
xiongjieyi <jinjingcs@gmail.com>
2020-08-03 16:06:13 +0200

    Updates

---
+++ precache-manifest.0c8611eda699af6276ad6470704844d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab52338e94423dd814dc7a23c33bb160",
+    "url": "/jr-assessment-jing/index.html"
+  },
+  {
+    "revision": "c3d57e2383e8422f8b7d",
+    "url": "/jr-assessment-jing/static/css/main.07a20f73.chunk.css"
+  },
+  {
+    "revision": "8ca438c12c029cd6cf7e",
+    "url": "/jr-assessment-jing/static/js/2.a1f345b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jr-assessment-jing/static/js/2.a1f345b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d57e2383e8422f8b7d",
+    "url": "/jr-assessment-jing/static/js/main.14cc3cf3.chunk.js"
+  },
+  {
+    "revision": "fc21a96d727e611b76f2",
+    "url": "/jr-assessment-jing/static/js/runtime-main.7ac16adb.js"
+  }
+]);
